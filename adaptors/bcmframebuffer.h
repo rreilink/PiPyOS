697b7f0b6955a78a16d2d4846a1e613e1c007b29
Rob Reilink <rob@reilink.net>
2018-01-05 20:35:44 +0100

    added missing bcmframebufer.h

---
+++ adaptors/bcmframebuffer.h
@@ -0,0 +1,7 @@
+#ifndef _BCMFRAMEBUFFER_H
+#define _BCMFRAMEBUFFER_H
+
+void PiPyOS_bcm_framebuffer_init(int width, int height);
+void PiPyOS_bcm_framebuffer_putstring(const char *s, int count);
+
+#endif
